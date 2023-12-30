8c01cf85c26cbd244e8e2a1cfd87a6de9695d03a
Avneesh Kumar <kaviikumar010@gmail.com>
2023-12-30 23:06:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar'
+import Header from './components/Header/Header'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div>
+     <Navbar/>
+     <Header/>
+    </div>
+    </>
+  )
+}
+
+export default App
